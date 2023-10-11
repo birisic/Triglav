360f87577929d353921587f527344f13198aff8e
Antea Primorac <antea.primorac0@gmail.com>
2023-10-11 11:23:03 +0200

    Added ShowPostPage.vue

---
+++ main/src/views/ShowPostPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <NoteView/>
+</template>
+
+<script>
+import NoteView from "../components/NoteView.vue";
+export default {
+  name: "ShowPost",
+  components: {
+    NoteView,
+  },
+};
+</script>
