25611d2f863a76136e89d18502ddcd7920844a5e
kanibalkorps <birisicmartin02@gmail.com>
2023-10-12 21:16:47 +0200

    Add post component

--- main/src/components/EditableField.vue
+++ main/src/components/EditableField.vue
@@ -1,14 +1,45 @@
 <template>
-  <div class="h-screen w-100">
-    <textarea
-      class="h-100 w-100"
-      name="userNote"
-      id="userNote"
-      v-model="content"
-      placeholder="Enter a note..."
-    ></textarea>
-    <v-btn @click="handleClick" class="position-fixed">{{ buttonLabel }}</v-btn>
-  </div>
+    <v-container>
+        <v-row class="justify-center">
+            <v-col :cols="10" class="elevation-2 my-16 h-100">
+                <v-form class="h-100">
+                    <v-text-field
+                            :counter="10"
+                            required
+                            hide-details
+                            bg-color="white"
+                            rounded="0"
+                            class="w-100 mx-auto elevation-0"
+                            placeholder="Title"
+                    ></v-text-field>
+                    <v-divider></v-divider>
+                    <!--        DESCRIPTION            -->
+                    <v-textarea
+                            :counter="10"
+                            rows="30"
+                            required
+                            hide-details
+                            bg-color="white"
+                            rounded="0"
+                            border="0"
+                            class="w-100 mx-auto elevation-0 h-100"
+                            placeholder="Enter your note for a post..."
+                    ></v-textarea>
+                    <v-btn @click="handleClick" class="position-fixed">{{ buttonLabel }}</v-btn>
+                </v-form>
+<!--                <div class="h-screen w-100">-->
+<!--                  <textarea-->
+<!--                          class="h-100 w-100"-->
+<!--                          name="userNote"-->
+<!--                          id="userNote"-->
+<!--                          v-model="content"-->
+<!--                          placeholder="Enter a note..."-->
+<!--                  ></textarea>-->
+<!--                    -->
+<!--                </div>-->
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -16,7 +47,9 @@ export default {
   data() {
     return {
       // Initialize with hardcoded text
-      content: "This is a hardcoded note.",
+      // content: "This is a hardcoded note.",
+        title: '',
+        desc: '',
     };
   },
   computed: {
