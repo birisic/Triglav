f5355ebf7dd5e8e5449964461d88012f2103ee0e
Žan Žerak <zan.zerak99@gmail.com>
2023-10-12 21:57:25 +0200

    added note saveing into array json

--- main/src/stores/DepartmentStore.js
+++ main/src/stores/DepartmentStore.js
@@ -18,7 +18,7 @@ export const useDepartmentStore = defineStore("DepartmentStore", {
     },
 
     getNoteData: (state) => (noteId) => {
-      return state.noteData.filter(note => note.id === parseInt(noteId));
+      return state.noteData.filter((note) => note.id === parseInt(noteId));
     },
   },
   //actions - methods
@@ -26,6 +26,10 @@ export const useDepartmentStore = defineStore("DepartmentStore", {
     toggleShowChildren(departmentId) {
       const currentValue = this.showChildrenMap.get(departmentId);
       this.showChildrenMap.set(departmentId, !currentValue);
+    },
+
+    addNote(newNote) {
+      this.noteData.unshift(newNote);
     },
   },
 });
